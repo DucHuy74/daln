4c0ceab90089628bc9cf89ad13cdc177d9df6710
DucHuy74 <duchuyhl74205@gmail.com>
2025-12-31 11:35:23 +0700

    feat: enhance workspace and backlog relationship; add BacklogResponse DTO and update Workspace entity

--- backend_mvc/src/main/java/com/xxxx/backend_mvc/dto/response/BacklogResponse.java
+++ backend_mvc/src/main/java/com/xxxx/backend_mvc/dto/response/BacklogResponse.java
@@ -1,4 +1,14 @@
 package com.xxxx.backend_mvc.dto.response;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class BacklogResponse {
+    String id;
+    String name;
 }

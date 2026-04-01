3b31a43de73c7bbb16bb148f1cdd9c05ee36ac53
DucHuy74 <duchuyhl74205@gmail.com>
2026-04-01 22:43:35 +0700

    feat: add GraphEventPort interface and GraphRebuildEvent class for event handling

---
+++ be/nckh/nckh-application/src/main/java/com/xxxx/ddd/application/port/async/GraphEventPort.java
@@ -0,0 +1,4 @@
+package com.xxxx.ddd.application.port.async;
+
+public interface GraphEventPort {
+}
---
+++ be/nckh/nckh-domain/src/main/java/com/xxxx/dddd/domain/model/graph/GraphRebuildEvent.java
@@ -0,0 +1,4 @@
+package com.xxxx.dddd.domain.model.graph;
+
+public class GraphRebuildEvent {
+}
